9ee7d8fa0682b68425e5b5cd895892d36dece092
Gaston <gastonbordon@gmail.com>
2022-11-01 01:14:56 -0300

    desafio 32

---
+++ Desafio Clase32/Desafio Clase32/routes/users/users.routes.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const authMiddleware = require("../../middlewares/auth/auth.middleware");
+
+const { Router } = express;
+const passport = require('../../middlewares/passport/passport.middleware')
+
+const router = Router();
+
+
+router.get("/login", authMiddleware, async (req, res) => {
+  return res.status(200).redirect("/api/productos");
+});
+
+
+router.post("/login", passport.authenticate('login', 
+  {failureRedirect: '/autherror'}
+), (req, res) => {
+  req.session.username = req.body.username;
+  req.session.admin = true;
+  res.status(200).redirect("/api/productos");
+});
+
+
+router.get("/signup", async (req, res) => {
+  res.render("signup", { layouts: "index" });
+});
+
+
+router.post("/signup", passport.authenticate('signup', {
+  failureRedirect: '/signuperror'
+}), (req, res) => {
+  req.session.username = req.body.username;
+  req.session.admin = true;
+  res.status(200).redirect("/api/productos");
+})
+
+
+router.get("/logout", authMiddleware, async (req, res) => {
+  try {
+    req.session.destroy((err) => {
+      if (err) {
+        return res.status(500).send(`<h1>No se pudo cerrar sesion</h1>`);
+      }
+    });
+    return res.status(200).redirect("/api/productos");
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+router.get("/autherror", (req,res)=>{
+  res.render("error", { layouts: "index", login: true });
+})
+
+router.get("/signuperror", (req,res)=>{
+  res.render("error", { layouts: "index", signup: true });
+})
+
+module.exports = router;
