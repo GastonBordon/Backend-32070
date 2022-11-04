f9ba35051f79ddb7383aeef7ad5143e3aaec559b
Gaston <gastonbordon@gmail.com>
2022-11-04 10:39:55 -0300

    desafio clase 34

--- Desafio Clase34/server.js
+++ Desafio Clase34/server.js
@@ -1,31 +1,26 @@
-const config = require('./config.js')
+const config = require("./config.js");
 require("dotenv").config();
 
-const cluster = require('cluster')
-const numCPUs = require('os').cpus().length;
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
-const {initSocket, httpServer} = require('./sockets/socket.config')
-initSocket()
+const { initSocket, httpServer } = require("./sockets/socket.config");
+initSocket();
 
+const PORT = process.env.PORT;
 
-const PORT = config.PORT;
+if (config.MODE.toUpperCase() === "CLUSTER" && cluster.isPrimary) {
+  console.log(`Primary process is running, id of process: ${process.pid}`);
 
-if((config.MODE).toUpperCase() === 'CLUSTER' && cluster.isPrimary){
+  for (let i = 1; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-console.log(`Primary process is running, id of process: ${process.pid}`)
-
-for (let i = 1; i< numCPUs; i++){
-cluster.fork()
-}
-
-cluster.on('exit', (worker,code,signal)=>{
-  console.log(`worker ${worker.process.pid} is offline`)
-})
-
-}else{
-
-httpServer.listen(PORT, () => {
-  console.info(`Server up and running on port ${PORT} - PID ${process.pid}`);
-
-});
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} is offline`);
+  });
+} else {
+  httpServer.listen(PORT, () => {
+    console.info(`Server up and running on port ${PORT} - PID ${process.pid}`);
+  });
 }
