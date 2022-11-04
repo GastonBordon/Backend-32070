128ccf8f8b25a9448277f00f977568c123318f0b
Gaston <gastonbordon@gmail.com>
2022-11-04 01:43:26 -0300

    desafio 32 terminado

--- Desafio Clase32/Desafio Clase32/config.js
+++
@@ -1,9 +0,0 @@
-const yargs =require('yargs/yargs')(process.argv.slice(2))
-const args = yargs.argv
-
-console.log(args)
-
-module.exports = {
-    MODE: args.mode || 'FORK',
-    PORT: args.port || 8080
-}
--- Desafio Clase32/Desafio Clase32/users.js
+++
@@ -1,3 +0,0 @@
-let Users = [{ id: 1, username: "admin", password: "$2b$10$pfMB/i2IQmPtNiACf4ApFethqtH/umvfjkw4wh/3qMwNeLsLj7kE6" }];
-
-module.exports = Users;
